34bbd936a0fa3917a2789f34bc8388e1610085f9
Delmar <delmar@umes.edu.gt>
2026-09-16 18:49:58 -0600

    feat: agrega ruta para crear usuario

--- core/urls.py
+++ core/urls.py
@@ -16,9 +16,23 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.http import JsonResponse
+from django.contrib.auth import get_user_model
+
+# --- TRUCO PARA CREAR EL ADMIN SIN CONSOLA ---
+def crear_admin_secreto(request):
+    User = get_user_model()
+    try:
+        if not User.objects.filter(username='delmar').exists():
+            User.objects.create_superuser('delmar', 'delmar@streak.com', 'Streak2026!')
+            return JsonResponse({'mensaje': '¡Admin creado con éxito!', 'usuario': 'delmar', 'clave': 'Streak2026!'})
+        return JsonResponse({'mensaje': 'El admin ya estaba creado. Ve a /admin/'})
+    except Exception as e:
+        return JsonResponse({'error_detectado': str(e)})
+# ---------------------------------------------
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('quinielas.urls')), # Conecta las rutas de tu app
-    
+    path('crear-admin/', crear_admin_secreto), # Nuestra ruta secreta
 ]
